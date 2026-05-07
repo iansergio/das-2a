a19ffe7aa6dc02edb06788b97d2d0d6b1e194409
pedromorikawa <127156227+pedromorikawa@users.noreply.github.com>
2026-05-07 20:57:40 -0300

    fix: letra errada

--- src/function_app.py
+++ src/function_app.py
@@ -8,7 +8,7 @@ from triggers.extract_estoque_movimentacao import app as extract_estoque_movimen
 from triggers.extract_estoque_saldo import app as extract_estoque_saldo
 from triggers.extract_pedido_item import app as extract_pedido_item
 from triggers.extract_pedido import app as extract_pedido
-from trigger.extract_produto import app as extract_produto
+from triggers.extract_produto import app as extract_produto
 from triggers.extract_regiao import app as extract_regiao
 from triggers.extract_representante import app as extract_representante
 from triggers.extract_titulo_receber import app as extract_titulo_receber
